d2415a2c6a6b8e1219a7fbccac7a9f500f2aaa29
Nikhil Swaminathan <nsswamin@amazon.com>
2019-06-19 11:00:21 -0700

    hosted ui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
+import { Auth } from 'aws-amplify'
+
+function App(props) {
   return (
     <div className="App">
       <header className="App-header">
@@ -10,17 +12,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => Auth.federatedSignIn()}>Sign In</button>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
